a5366778f6bc16dbe06f31ad7906f78762dd7b7a
Smita Srivastava <Smita.Srivastava@jda.com>
2021-02-21 17:48:45 +0530

    Updated for taing input.

--- src/HelloWorldClass.java
+++ src/HelloWorldClass.java
@@ -1,6 +1,17 @@
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 public class HelloWorldClass {
 
     public static void main(String args[]){
-        System.out.println("hello world there !!");
+        System.out.println("hello world there !!" + " may name is : "+getName()+ " !!");
+    }
+
+    public static String getName(){
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        return name;
     }
 }
